69ab8c9bfccf557573f69b762e6207d0f8f31b3e
Ben Lee <benlee@nuevaschool.org>
2018-12-01 20:04:43 -0800

    Added platforming mechanics, support for potential multiple jumps fixed bullets not appearing
    BUG:
    bullet firing direction based off of pixel mouse position, meaning it's not based off of the player's location but rather the bottom left corner of the window
    
    Signed-off-by: Ben Lee <benlee@nuevaschool.org>

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour {
 
+    public Rigidbody2D rb;
+
 	float speed = 5.0f;
 	int health = 5;
+    int maxJumps = 1;
+    int jumps = 1;
+    bool justJumped = false;
 	List<Ability> abilities = new List<Ability>();
 
 	// Use this for initialization
@@ -15,12 +20,29 @@ public class PlayerScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
-     	transform.position += move * speed * Time.deltaTime;
+        print(Input.GetAxis("Vertical") + " / " + jumps + " / " + justJumped);
+		Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        if(Input.GetAxis("Vertical") > 0 && jumps > 0 && justJumped == false) {
+            rb.AddForce(new Vector2(0, 2), ForceMode2D.Impulse);
+            jumps -= 1;
+        } else {
+            justJumped = false;
+        }
+        transform.position += move * speed * Time.deltaTime;
 		foreach(Ability a in abilities) {
 			if(Input.GetKeyDown(a.castKey)) {
 				a.activate(transform.position, Input.mousePosition, gameObject);
 			}
 		}
 	}
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        print("Collision");
+        if (collision.gameObject.CompareTag("Ground")) {
+            jumps = maxJumps;
+            justJumped = false;
+        }
+    }
+
+
 }
